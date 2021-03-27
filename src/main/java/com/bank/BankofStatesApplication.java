27840f779e423faacda2a3c799e8bc8ce9f2e36e
Mohammed Riyaz <jmriyaz84@gmail.com>
2021-03-27 18:38:32 +0400

    SSL

--- src/main/java/com/bank/BankofStatesApplication.java
+++ src/main/java/com/bank/BankofStatesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankofStatesApplication {
 
 	public static void main(String[] args) {
+System.setProperty("server.servlet.context-path", "/bankofStates");
 		SpringApplication.run(BankofStatesApplication.class, args);
 	}
 
